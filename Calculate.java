82d41e6b94ea1446487f343d11d5d03970626216
Yaswanth Borra <yaswanth_kumar_borra@epam.com>
2022-03-15 22:29:05 +0530

    initial commit

---
+++ Calculate.java
@@ -0,0 +1,11 @@
+public class Calculate{
+    void add(int a,int b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        Calculate c=new Calculate();
+        c.add(10, 20);
+    }
+
+}
